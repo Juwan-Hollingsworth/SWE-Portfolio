833647aa819178f55ca06ce258c2536c6d94bef7
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-10 17:03:56 -0400

    fix navbar positioning

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -8,45 +8,50 @@ import arrow from "../../assets/arrow.png";
 function Home() {
   return (
     <div className="home_main">
-      <div className="home_nav">
-        <Navbar />
-      </div>
-      <div className="home_hero">
-        <div className="home_row">
-          <div className="home_col 1">
-            <h1 id="home_title">Juwan Hollingsworth. </h1>
-            <br />
-            <div class="flipping_words" id="flipping_words_container">
-              <span className="flipping_w1"> Freelance Software Engineer</span>
-              <span className="flipping_w2">
-                Professional Software Engineer
-              </span>
-              <span className="flipping_w3">Hobbyist Software Engineer</span>
+      <div className="home_container">
+        <div className="home_nav">
+          <Navbar />
+        </div>
+        <div className="home_hero">
+          <div className="home_row">
+            <div className="home_col 1">
+              <h1 id="home_title">Juwan Hollingsworth. </h1>
+              <br />
+              <div class="flipping_words" id="flipping_words_container">
+                <span className="flipping_w1">
+                  {" "}
+                  Freelance Software Engineer
+                </span>
+                <span className="flipping_w2">
+                  Professional Software Engineer
+                </span>
+                <span className="flipping_w3">Hobbyist Software Engineer</span>
+              </div>
+              <span class="home_static_h2" id="static_h2"></span> <br />
+              <br />
+              <span id="location">Atlanta, Georgia 📍</span>
+              <br />
             </div>
-            <span class="home_static_h2" id="static_h2"></span> <br />
-            <br />
-            <span id="location">Atlanta, Georgia 📍</span>
-            <br />
-          </div>
-          <div className="home_col 2">
-            <div class="hero_img_container">
-              <img src={heroimg} alt="hero image" className="hero__img" />
+            <div className="home_col 2">
+              <div class="hero_img_container">
+                <img src={heroimg} alt="hero image" className="hero__img" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="home_arrow">
-        {" "}
-        <AnchorLink
-          activeClass="active"
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <img className="arrow" src={arrow} />
-        </AnchorLink>
+        <div className="home_arrow">
+          {" "}
+          <AnchorLink
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <img className="arrow" src={arrow} />
+          </AnchorLink>
+        </div>
       </div>
     </div>
   );
