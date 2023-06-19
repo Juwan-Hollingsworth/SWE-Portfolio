883cbeacfa8daa04cec3052f6b2c3f0b3c0931a8
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-19 17:28:20 -0400

    fix minor css issues

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -7,10 +7,13 @@ function Contact() {
   return (
     <div className="contact_main">
       <h2 className="contact_title">Let's Connect 👋</h2>
-      <button className="ghostBtn_contact">Email 📩</button>
-      <button className="ghostBtn_contact">LinkedIn 👨🏾‍💻</button>
-      <button className="ghostBtn_contact">Github 👾</button>
-      <button className="ghostBtn_contact">Spark ⚡️</button>
+      <div className="contact_items">
+        <button className="ghostBtn_contact">Email 📩</button>
+        <button className="ghostBtn_contact">LinkedIn 👨🏾‍💻</button>
+        <button className="ghostBtn_contact">Github 👾</button>
+        <button className="ghostBtn_contact">Spark ⚡️</button>
+      </div>
+
       <br />
       <br />
       <br />
