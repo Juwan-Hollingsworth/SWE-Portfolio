d8fada47fe0449e787742946781345b87b3cd7bd
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-20 01:01:19 -0400

    add banner files

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./banner.css"; // Import the CSS file for styling
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-text">
+        <span className="moving-text">Banner</span>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
